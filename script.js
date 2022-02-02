d5ebe1a9c153143d99f34a0e3d406f59457e3264
Vidotti <vidottizzz@gmail.com>
2022-02-02 15:40:06 -0300

    finished the project

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const inputEmail = document.querySelector('.inputEmail');
 const buttonEmail = document.querySelector('.buttonEmail');
+const errMsg = document.querySelector('.err-msg');
+
+let validRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
 
 buttonEmail.addEventListener('click', validação);
 
+
+  
 function validação() {
-    if(inputEmail.value.trim() === '') {
+    if(inputEmail.value.trim() === '' || !inputEmail.value.match(validRegex)) {
         inputEmail.classList.add('border-red');
+        errMsg.classList.remove('hide');
      }
      else {
         inputEmail.classList.remove('border-red');
+        errMsg.classList.add('hide');
      }
 }
